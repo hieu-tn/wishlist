7fe84116ecd3015ef802c440577c90316560f35b
hieu-tn <vayu411@gmail.com>
2023-05-01 01:57:42 -0600

    toast stack

--- src/components/common/Toast.tsx
+++ src/components/common/Toast.tsx
@@ -1,17 +1,19 @@
 import { useState } from "react"
-import { Alert } from "@mui/material"
+import { Alert, Grow } from "@mui/material"
 
 
 export default function Toast() {
   const [open, setOpen] = useState(true)
 
-  const handleClose = (idx: number): void => {
+  const handleClose = (): void => {
     setOpen(false)
   }
 
   return (
-    <Alert onClose={ () => handleClose(1) } severity="success" sx={ {width: "100%"} }>
-      This is a success message!
-    </Alert>
+    <Grow in={ open }>
+      <Alert onClose={ handleClose } severity="success" sx={ {width: "100%"} }>
+        This is a success message!
+      </Alert>
+    </Grow>
   )
 }
