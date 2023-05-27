3d01411772f2e571a3855a85e15499982db81985
hieu-tn <vayu411@gmail.com>
2023-05-26 23:52:37 -0600

    sticky total price

--- src/modules/wishlists/components/WishlistItem.tsx
+++ src/modules/wishlists/components/WishlistItem.tsx
@@ -61,7 +61,10 @@ export default function WishlistItem({id}: WishlistItemProps) {
           </Accordion>
         </Grid>
         <Grid item xs={ 4 }>
-          <Box sx={ {flexGrow: 1, border: `1px solid ${ blueGrey[300] }`, p: 2, pb: 4, borderRadius: 3} }>
+          <Box sx={ {
+            flexGrow: 1, border: `1px solid ${ blueGrey[300] }`, p: 2, pb: 4, borderRadius: 3,
+            position: "sticky", top: "8px",
+          } }>
             <Typography variant="h5" sx={ {color: purple[800], pb: 2} }><strong>{ data?.name }</strong></Typography>
             <Box>
               <Grid container spacing={ 1 }>
